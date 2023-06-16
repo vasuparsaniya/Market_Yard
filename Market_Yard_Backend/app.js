3f1c935980fd4157420aa4263175dd62f2b1ff8f
vasu <vasu@gmail.com>
2023-06-16 20:14:45 +0530

    Book Slot

--- Market_Yard_Backend/app.js
+++ Market_Yard_Backend/app.js
@@ -1,9 +1,16 @@
 var http = require('http');
 var express = require('express');
-var path = require('path');
+
 var app = express();
 var databaseUrl = 'mongodb://localhost:27017/market_yard';
-var collections = ['farmer','users','retailer'];
+var collections = ['farmer', 'users', 'retailer','bookSlot'];
+
+const uuid = require('uuid');
+const ejs = require('ejs');
+const path = require('path');
+const pdf = require('html-pdf');
+const fs = require('fs');
+
 
 var mongojs = require('mongojs');
 var db = mongojs(databaseUrl, collections);
@@ -39,6 +46,33 @@ app.get('/getReatiler', function (req, res) {
     });
 });
 
+// app.get('/getRetailer/:user_id', function (req, res) {
+//     var userId = req.params.user_id;
+//     db.retailer.findOne({ _id: mongojs.ObjectId(userId) }, function (err, retailer) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(retailer); // Log the retailer object
+//             res.json(retailer);
+//         }
+//     });
+// });
+
+app.get('/getRetailer/:user_id', function (req, res) {
+    var userId = req.params.user_id;
+    db.retailer.findOne({ _id: mongojs.ObjectId(userId) }, function (err, retailer) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(retailer);
+            res.json(retailer);
+        }
+    });
+});
+
+
+
+
 
 //---------------------farmer-------------------------------------
 app.post('/insertFarmer', function (req, res) {
@@ -55,7 +89,7 @@ app.post('/insertFarmer', function (req, res) {
 //----------------------retailer----------------------------------
 app.post('/insertRetailer', function (req, res) {
     var jsonData = req.body;
-    db.retailer.save({ name: jsonData.name, role: jsonData.role, price: jsonData.price, gender: jsonData.gender, crop: jsonData.crop, date: jsonData.date, phone: jsonData.phone, email: jsonData.email, }, function (err, saved) {
+    db.retailer.save({ name: jsonData.name, role: jsonData.role, price: jsonData.price, gender: jsonData.gender, crop: jsonData.crop, date: jsonData.date, phone: jsonData.phone, address: jsonData.address, email: jsonData.email, }, function (err, saved) {
         if (err) {
             console.log(err);
         } else {
@@ -63,6 +97,255 @@ app.post('/insertRetailer', function (req, res) {
         }
     });
 });
+
+//---------------------Book Slot-------------------------------------
+// app.post('/insertBookSlot', function (req, res) {
+//     var jsonData = req.body;
+//     db.bookSlot.save({ farmerName: jsonData.farmerName, gender: jsonData.gender, farmerPhone: jsonData.farmerPhone, farmerEmail: jsonData.farmerEmail, slotTime: jsonData.slotTime, date: jsonData.date,
+//          retailerName: jsonData.retailerName, retailerPhone: jsonData.retailerPhone, crop: jsonData.crop, retailerPhone: jsonData.retailerPhone, address: jsonData.address }, function (err, saved) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.end('Slot Booked');
+
+//             var savedSlot = { 
+//                 farmerName: jsonData.farmerName, gender: jsonData.gender, farmerPhone: jsonData.farmerPhone, farmerEmail: jsonData.farmerEmail, 
+//                 slotTime: jsonData.slotTime, date: jsonData.date, retailerName: jsonData.retailerName, retailerPhone: jsonData.retailerPhone, 
+//                 crop: jsonData.crop, retailerPhone: jsonData.retailerPhone, address: jsonData.address }
+
+//             // Generate the PDF with the saved slot data
+//       generatePDF(savedSlot)
+//         .success((pdfBytes) => {
+//           // Send the PDF as a response
+//           res.setHeader('Content-Type', 'application/pdf');
+//           res.setHeader('Content-Disposition', 'attachment; filename="slot.pdf"');
+//           res.send(pdfBytes);
+//         })
+//         .error((error) => {
+//           console.log('Error generating PDF:', error);
+//           res.status(500).json({ error: 'Server error' });
+//         });
+//         }
+//     });
+// });
+
+// // Function to generate the PDF with the provided slot data
+// async function generatePDF(slotData) {
+//   // Load an existing PDF or create a new one
+//   const pdfDoc = await PDFDocument.create();
+
+//   // Set the font and font size for the PDF content
+//   const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+//   const fontSize = 12;
+
+//   // Create a new page in the PDF
+//   const page = pdfDoc.addPage();
+//   const { width, height } = page.getSize();
+
+//   // Define the content to be written in the PDF
+//   const content = `
+//     Farmer Name: ${slotData.farmerName}
+//     Gender: ${slotData.gender}
+//     Farmer Phone: ${slotData.farmerPhone}
+//     Farmer Email: ${slotData.farmerEmail}
+//     Date: ${slotData.date}
+//     Retailer Name: ${slotData.retailerName}
+//     Retailer Phone: ${slotData.retailerPhone}
+//     Crop: ${slotData.crop}
+//     Address: ${slotData.address}
+//   `;
+
+//   // Write the content to the PDF
+//   page.drawText(content, {
+//     x: 50,
+//     y: height - 50,
+//     size: fontSize,
+//     font: font,
+//     color: rgb(0, 0, 0),
+//   });
+
+//   // Save the PDF as a byte array
+//   const pdfBytes = await pdfDoc.save();
+
+//   return pdfBytes;
+// }
+
+//--------------------------------------------------------------------------------------------------------------
+
+// app.post('/insertBookSlot', function (req, res) {
+//   const generatedUuid = uuid.v1();
+//   var jsonData = req.body;
+
+//   db.bookSlot.save({
+//     farmerName: jsonData.farmerName,
+//     gender: jsonData.gender,
+//     farmerPhone: jsonData.farmerPhone,
+//     farmerEmail: jsonData.farmerEmail,
+//     slotTime: jsonData.slotTime,
+//     date: jsonData.date,
+//     retailerName: jsonData.retailerName,
+//     retailerPhone: jsonData.retailerPhone,
+//     crop: jsonData.crop,
+//     address: jsonData.address
+//   }, function (err, saved) {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).json({ error: 'Server error' });
+//     } else {
+//       const templatePath = path.join(__dirname, 'routes', 'report.ejs');
+//       const outputPath = path.join(__dirname, './generated_pdf/', generatedUuid + '.pdf');
+
+//       ejs.renderFile(templatePath, {
+//         farmerName: jsonData.farmerName,
+//         gender: jsonData.gender,
+//         farmerPhone: jsonData.farmerPhone,
+//         farmerEmail: jsonData.farmerEmail,
+//         slotTime: jsonData.slotTime,
+//         date: jsonData.date,
+//         retailerName: jsonData.retailerName,
+//         retailerPhone: jsonData.retailerPhone,
+//         crop: jsonData.crop,
+//         address: jsonData.address
+//       }, (err, html) => {
+//         if (err) {
+//           console.log(err);
+//           return res.status(500).json(err);
+//         } else {
+//           pdf.create(html).toFile('./generated_pdf/' + generatedUuid + ".pdf", function (err, result) {
+//             if (err) {
+//               console.log(err);
+//               return res.status(500).json(err);
+//             } else {
+//               res.setHeader('Content-Type', 'application/pdf');
+//             //   res.setHeader('Content-Disposition', 'attachment; filename=' + generatedUuid + '.pdf');
+//             res.send(result);
+//             //   res.sendFile(outputPath);
+//             }
+//           });
+//         }
+//       });
+//     }
+//   });
+// });
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+const nodemailer = require('nodemailer');
+
+
+app.post('/insertBookSlot', (req, res) => {
+  const generatedUuid = uuid.v1();
+  const jsonData = req.body;
+
+  db.bookSlot.save({
+    farmerName: jsonData.farmerName,
+    gender: jsonData.gender,
+    farmerPhone: jsonData.farmerPhone,
+    farmerEmail: jsonData.farmerEmail,
+    slotTime: jsonData.slotTime,
+    date: jsonData.date,
+    retailerName: jsonData.retailerName,
+    retailerPhone: jsonData.retailerPhone,
+    crop: jsonData.crop,
+    address: jsonData.address
+  }, (err, saved) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Server error' });
+    } else {
+        
+      const templatePath = path.join(__dirname, 'routes', 'report.ejs');
+      const outputPath = path.join(__dirname, './generated_pdf/', generatedUuid + '.pdf');
+
+      ejs.renderFile(templatePath, {
+        farmerName: jsonData.farmerName,
+        gender: jsonData.gender,
+        farmerPhone: jsonData.farmerPhone,
+        farmerEmail: jsonData.farmerEmail,
+        slotTime: jsonData.slotTime,
+        date: jsonData.date,
+        retailerName: jsonData.retailerName,
+        retailerPhone: jsonData.retailerPhone,
+        crop: jsonData.crop,
+        address: jsonData.address
+      }, (err, html) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        } else {
+          pdf.create(html).toFile(outputPath, (err, result) => {
+            if (err) {
+              console.log(err);
+              return res.status(500).json(err);
+            } else {
+              // Send the email with the PDF attachment
+              const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                host: 'smtp.google.email',
+                // host: 'smtp.ethereal.email',
+                port: 587,
+                secure: false,
+                auth: {
+                    user: 'vasuparsaniya21@gmail.com',
+                    pass: 'vwkwjanzdtbdorae'
+                }              
+            });
+
+              const mailOptions = {
+                from: 'vasuparsaniya21@gmail.com', // Sender email address
+                to: jsonData.farmerEmail, // Farmer email address
+                subject: 'Market Yard Book Slot Confirmation',
+                text: 'Please find the attached PDF for your slot confirmation.',
+                attachments: [
+                  {
+                    filename: generatedUuid + '.pdf',
+                    path: outputPath
+                  }
+                ]
+              };
+
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  console.log(error);
+                  return res.status(500).json({ error: 'Failed to send email' });
+                } else {
+                  console.log('Email sent:', info.response);
+                  res.json({ success: true, message: 'PDF generated and email sent successfully' });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+
+
+// app.get('/getRetailer/:retailerId', (req, res) => {
+//     const retailerId = req.params.retailerId;
+
+//     try {
+//       // Find the retailer in the database by ID
+//       const Retailer = retailer.findById(retailerId).select('date name phone crop address');
+
+//       if (Retailer) {
+//         res.json(Retailer); // Return the retailer data as JSON
+//       } else {
+//         res.status(404).json({ error: 'Retailer not found' }); // Retailer not found
+//       }
+//     } catch (error) {
+//       res.status(500).json({ error: 'Server error' }); // Handle server error
+//     }
+//   });
+
+
+
+
+
+
+
 
 
 // app.put('/updateUser', function (req, res) {
@@ -116,7 +399,7 @@ app.post('/login', function (req, res) {
 app.post('/signup', function (req, res) {
     var jsonData = req.body;
 
-    db2.users.findOne({ email: jsonData.email}, function (err, user) {
+    db2.users.findOne({ email: jsonData.email }, function (err, user) {
         if (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
--- Market_Yard_Backend/routes/index.js
+++ Market_Yard_Backend/routes/index.js
@@ -6,13 +6,14 @@ app.config(['$httpProvider', function ($httpProvider) {
 }
 ]);
 
-app.controller('UserListCtrl', ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache) {
+app.controller('UserListCtrl', ['$scope', '$http', '$templateCache', '$window', function($scope, $http, $templateCache, $window,$location) {
 // ...
 var serverUrl = 'http://localhost:8080';
 var insertFarmerUrl = serverUrl + '/insertFarmer';
 var insertRetailerUrl = serverUrl + '/insertRetailer';
+var insertBookSlotUrl = serverUrl + '/insertBookSlot';
 var getUrl = serverUrl + '/getReatiler';
-
+var getNewUrl = serverUrl + '/getRetailer/';
 
 var insertMethod = 'POST';
 var getMethod = 'GET';
@@ -25,7 +26,7 @@ $scope.crops = ['Ragi', 'Wheat', 'Rice', 'Jowar', 'Bajra', 'Pulses'];
 $scope.farmer = function () {
     var formData = {
         name: this.name,
-        role: this.role,
+        role: 'Farmer',
         gender: this.gender,
         crop: this.crop,
         date: this.date,
@@ -48,6 +49,9 @@ $scope.farmer = function () {
     })
         .success(function (response) {
             console.log("Farmer Successfully register....")
+            var message = "Farmer Successfully register....";
+            alert(message);
+            $window.location.assign('data.html'); // Redirect to data.html
             // $scope.list();
         })
         .error(function (error) {
@@ -60,12 +64,13 @@ $scope.farmer = function () {
 $scope.retailer = function () {
     var formData = {
         name: this.name,
-        role: this.role,
+        role: 'Retailer',
         price:this.price,
         gender: this.gender,
         crop: this.crop,
         date: this.date,
         phone: this.phone,
+        address:this.address,
         email: this.email
     };
     this.name = '';
@@ -75,6 +80,7 @@ $scope.retailer = function () {
     this.crop = '';
     this.date = '';
     this.phone = '';
+    this.address = '';
     this.email = '';
 
     $http({
@@ -85,6 +91,10 @@ $scope.retailer = function () {
     })
         .success(function (response) {
             console.log("Retailer Successfully register....")
+
+            var message = "Retailer Successfully register....";
+            alert(message);
+            $window.location.assign('index.html'); // Redirect to data.html
             // $scope.list();
         })
         .error(function (error) {
@@ -93,9 +103,6 @@ $scope.retailer = function () {
 
     return false;
 };
-
-
-
 
 $scope.list = function() {
     $http.get(getUrl)
@@ -106,6 +113,203 @@ $scope.list = function() {
             console.log(error);
         });
 };
+
+// $scope.list1 = function() {
+//     $http.get(getUrl)
+//       .success(function(response) {
+//         // Assuming the response data is in the following format:
+//         // response.data = {
+//         //   retailerName: "Retailer Name",
+//         //   currentAddress: "Current Address"
+//         // }
+  
+//         // Populate the retailer name and current address in the form
+//         $scope.retailerName = response.data.name;
+//         $scope.retailerPhone = response.data.phone;
+//         $scope.crop = response.data.crop;
+//         $scope.date= response.data.date;
+//         $scope.currentAddress = response.data.address;
+//       })
+//       .error(function(error) {
+//         console.log(error);
+//       });
+//   };
+  
+
+
+//-----------old---------
+// $scope.getRetailerData = function() {
+//     var searchParams = new URLSearchParams(window.location.search);
+//     var userId = searchParams.get('user_id');
+
+//     $http.get(getNewUrl + userId)
+//       .success(function(response) {
+//         if (response.data && response.data.date) {
+//           $scope.date = response.data.date;
+//           $scope.retailerName = response.data.name;
+//           $scope.retailerPhone = response.data.phone;
+//           $scope.crop = response.data.crop;
+//           $scope.address = response.data.address;
+//         } else {
+//           console.log('Invalid retailer data:', response.data);
+//           console.log(response.data);
+//         }
+//       })
+//       .error(function(error) {
+//         console.log('Error fetching retailer data:', error);
+//       });
+// };
+
+$scope.getRetailerData = function() {
+    var searchParams = new URLSearchParams(window.location.search);
+    var retailerId = searchParams.get('user_id');
+  
+    $http.get(getNewUrl + retailerId)
+      .success(function(response) {
+        if (response.date) {
+          $scope.date = response.date;
+          $scope.retailerName = response.name;
+          $scope.retailerPhone = response.phone;
+          $scope.crop = response.crop;
+          $scope.address = response.address;
+        } else {
+          console.log('Invalid retailer data:', response);
+        }
+      })
+      .error(function(error) {
+        console.log('Error fetching retailer data:', error);
+      });
+  };
+  
+  $scope.getQueryParam = function(param) {
+    var searchParams = new URLSearchParams(window.location.search);
+    return searchParams.get(param);
+  };
+  
+
+    $scope.getQueryParam = function(param) {
+        var searchParams = new URLSearchParams(window.location.search);
+        return searchParams.get(param);
+    };
+  
+    
+    //---------------------------------------------------------------------------------------------
+$scope.bookSlot = function () {
+    var formData = {
+        farmerName: this.farmerName,
+        gender:this.gender,
+        farmerPhone: this.farmerPhone,
+        farmerEmail: this.farmerEmail,
+        slotTime: this.slotTime,
+        date:this.date,
+        retailerName:this.retailerName,
+        retailerPhone:this.retailerPhone,
+        crop: this.crop,
+        retailerPhone: this.retailerPhone,
+        address: this.address
+    };
+    this.farmerName = '';
+    this.gender = '';
+    this.farmerPhone = '';
+    this.farmerEmail = '';
+    this.slotTime = '';
+    this.date = '';
+    this.retailerName = '';
+    this.retailerPhone = '';
+    this.crop = '';
+    this.retailerPhone = '';
+    this.address = '';
+
+    $http({
+        method: insertMethod,
+        url: insertBookSlotUrl,
+        data: formData, // Send data in the request body as JSON
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .success(function (response) {
+            console.log("Slot Book Successfully....")
+            var message = "Slot Book Successfully....";
+            alert(message);      
+            // $window.location.assign('index.html'); // Redirect to data.html
+            // $scope.list();
+
+            // Trigger the download
+    //   var link = document.createElement('a');
+    //   link.href = "data:application/pdf;base64," + response;
+    //   link.download = 'generated_pdf.pdf';
+    //   link.click();
+
+    // var blob = new Blob([response.data], { type: 'application/pdf' });
+    // saveAs(blob, 'generated_pdf.pdf');
+        })
+        .error(function (error) {
+            console.log(error);
+        });
+
+    return false;
+};
+
+
+// $scope.bookSlot = function () {
+//     var formData = {
+//       farmerName: this.farmerName,
+//       gender: this.gender,
+//       farmerPhone: this.farmerPhone,
+//       farmerEmail: this.farmerEmail,
+//       slotTime: this.slotTime,
+//       date: this.date,
+//       retailerName: this.retailerName,
+//       retailerPhone: this.retailerPhone,
+//       crop: this.crop,
+//       retailerPhone: this.retailerPhone,
+//       address: this.address
+//     };
+  
+//     // Clear form fields
+//     this.farmerName = '';
+//     this.gender = '';
+//     this.farmerPhone = '';
+//     this.farmerEmail = '';
+//     this.slotTime = '';
+//     this.date = '';
+//     this.retailerName = '';
+//     this.retailerPhone = '';
+//     this.crop = '';
+//     this.retailerPhone = '';
+//     this.address = '';
+  
+//     $http({
+//       method: insertMethod,
+//       url: insertBookSlotUrl,
+//       data: formData,
+//       headers: { 'Content-Type': 'application/json' },
+//       responseType: 'arraybuffer' // Specify the response type as arraybuffer
+//     })
+//     .success(function (response) {
+//         var message = "Slot Book Successfully....";
+//         alert(message);
+//         var blob = new Blob([response.data], { type: 'application/pdf' });
+
+//         // Create a temporary URL for the PDF file
+//         var url = window.URL.createObjectURL(blob);
+  
+//         // Create a link element and simulate a click to trigger the download
+//         var link = document.createElement('a');
+//         link.href = url;
+//         link.download = 'generated_pdf.pdf';
+//         link.click();
+  
+//         // Clean up the temporary URL after the download
+//         window.URL.revokeObjectURL(url);
+//       })
+//       .error(function (error) {
+//         console.log(error);
+//       });
+
+  
+//     return false;
+//   };
+  
 }]);
 
 
